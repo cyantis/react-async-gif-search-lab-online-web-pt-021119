31c7ffb217a9b9f94444e0c687bbd7e55e2e0c56
cyantis <cyantis@gmail.com>
2019-11-04 21:05:42 -0700

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const GifList = () => {
+export default class GifList extends React.Component {
+
+createList = () => {
   let list = [];
 
-  //for(let i of this.props.imgList){
-  //  list.push(<li><img src={ i.images.original.url } /></li>)
-  //}
-
-  return(
-    <ul>
-      { this.list }
-    </ul>
-  )
-
+  for(let i of this.props.imgList){
+    list.push(<li><img src={ i.images.original.url } /></li>)
+  }
+  return list;
 }
 
-export default GifList;
+  render(){
+    return(
+      <ul>
+        { this.createList() }
+      </ul>
+    )
+  }
+}
